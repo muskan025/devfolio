17062837b78062934216ac8a5217c2f6397679e7
muskan025 <muskandodmani760@gmail.com>
2026-05-23 05:44:29 +0530

    feat: install acertinity timeline

--- src/ProjectTimeline.tsx
+++ src/ProjectTimeline.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
  
 import {timelineData} from "./timelineData"
+import TimelineDemo from './components/timeline-demo'
 
 
 export const ProjectTimeline = () => {
   return (
-    <Timeline data={timelineData} />
+    <TimelineDemo data={timelineData} />
   )
 }
